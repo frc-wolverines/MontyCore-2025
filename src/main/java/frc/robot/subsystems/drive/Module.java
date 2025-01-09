f9e3395786302a5b38e47ba281a537d0fbbadff9
frc-wolverines <rosemont5274@gmail.com>
2025-01-08 17:19:35 -0800

    Added Functions For Drive/Pivot

--- src/main/java/frc/robot/subsystems/drive/Module.java
+++ src/main/java/frc/robot/subsystems/drive/Module.java
@@ -2,9 +2,12 @@ package frc.robot.subsystems.drive;
 
 import javax.print.attribute.standard.PresentationDirection;
 
+import com.ctre.phoenix.sensors.CANCoder;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.fasterxml.jackson.core.io.IOContext;
+
+import edu.wpi.first.wpilibj.CAN;
 
 //TODO: Writing Periodic outputs
 //TODO: Telemetry/ Odometry
@@ -27,5 +30,32 @@ public class Module {
         public double demandedDrivePosition = 0.0;
         public double demandedDriveVelocity = 0.0;
         
+    }
+
+    public Module(ModuleConfiguration configuration) {
+        driveMotor = new TalonFX(configuration.driveMotorID);
+        pivotMotor = new TalonFX(configuration.pivotMotorID);
+        pivotEncoder = new CANcoder(configuration.canCoderID);
+    }
+
+    public double getDrivePosition() {
+        return driveMotor.getPosition().getValueAsDouble();
+    }
+    public double getDriveVelocity() {
+        return driveMotor.getVelocity().getValueAsDouble();
+    }
+    public double getPivotPosition() {
+        return pivotMotor.getPosition().getValueAsDouble();
+    }
+    public static class ModuleConfiguration {
+        public final int driveMotorID;
+        public final int pivotMotorID;
+        public final int canCoderID;
+
+        public ModuleConfiguration(int driveMotorID, int pivotMotorID, int canCoderID) {
+            this.driveMotorID = driveMotorID;
+            this.pivotMotorID = pivotMotorID;
+            this.canCoderID = canCoderID;
+        }
     }
 }
