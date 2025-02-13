d0ef493e8781c97cf2c487724347ef256568ab11
debusk.elijah <debusk.elijah@gmail.com>
2025-02-13 17:03:12 +0000

    implemented javadoc descriptions

--- MontyCore 2025/src/main/java/team5274/robot/subsystems/drive/SwerveModule.java
+++ MontyCore 2025/src/main/java/team5274/robot/subsystems/drive/SwerveModule.java
@@ -41,38 +41,74 @@ public class SwerveModule {
         controller.enableContinuousInput(-1, 1);
     }
 
+    /**
+     * Retrieves the module's number
+     * @return the module's number
+     */
     public int getNumber() {
         return moduleNumber;
     }
 
+    /**
+     * Retrieves the wheel's relative position
+     * @return the wheels z-axis rotation
+     */
     public double getPivotPosition() {
         return pivotMotor.getPosition().getValueAsDouble() * DriveConstants.kPivotGearRatio;
     }
 
+    /**
+     * Retrieves the wheel's traveled position in meters
+     * @return the wheels traveled distance
+     */
     public double getTrackPosition() {
         return driveMotor.getPosition().getValueAsDouble() * DriveConstants.kDriveGearRatio * DriveConstants.kTrackCircumference;
     }
 
+    /**
+     * Retrieves the wheel's velocity in meters per second
+     * @return the wheel's current velocity
+     */
     public double getTrackVelocity() {
         return driveMotor.getVelocity().getValueAsDouble() * DriveConstants.kDriveGearRatio * DriveConstants.kTrackCircumference;
     }
 
+    /**
+     * Retrieves the wheel's Rotation2d value
+     * @return the wheel's rotation in the form of a Rotation2d instance
+     */
     public Rotation2d getPivotRot2d() {
         return Rotation2d.fromRotations(getPivotPosition());
     }
 
+    /**
+     * Retrieves the angular position given by the absolute encoder. This value is inverted to properly read input for the MK4i series swerve modules from SDS.
+     * @return the wheel's absolute angle
+     */
     public double getAbsPivotPosition() {
         return -encoder.getAbsolutePosition().getValueAsDouble();
     }
 
+    /**
+     * Retrieves the SwerveModuleState based on the wheel's velocity and rotation
+     * @return an instance of SwerveModuleState
+     */
     public SwerveModuleState getState() {
         return new SwerveModuleState(getTrackVelocity(), getPivotRot2d());
     }
 
+    /**
+     * Retrieves the SwerveModulePosition based on the wheel's position and rotation
+     * @return an instance of SwerveModulePosition
+     */
     public SwerveModulePosition getPosition() {
         return new SwerveModulePosition(getTrackPosition(), getPivotRot2d());
     }
 
+    /**
+     * Sets the desired SwerveModuleState that the module should run at
+     * @param state an instance of SwerveModuleState
+     */
     public void setDesiredState(SwerveModuleState state) {
         
         //Discards small speeds
@@ -91,21 +127,24 @@ public class SwerveModule {
         );
     }
 
+    /**Resets the encoders' positions to 0 */
     public void resetEncoders() {
         driveMotor.setPosition(0.0);
         pivotMotor.setPosition(0.0);
     }
 
+    /**Stops all motor output */
     public void stop() {
         driveMotor.stopMotor();
         pivotMotor.stopMotor();
     }
 
-    /**Zeros the pivot encoder's position to the current absolute encoders position*/
+    /**Zeros the pivot encoder's position to the current absolute encoders position */
     public void zeroPivotPosition() {
         pivotMotor.setPosition(getAbsPivotPosition());
     }
 
+    /**A class to store TalonFXConfigurations for the swerve module */
     public static class SwerveModuleConfiguration {
         public final TalonFXConfiguration driveConfiguration, pivotConfiguration;
 
@@ -115,6 +154,7 @@ public class SwerveModule {
         }
     }
 
+    /**A class to store DeviceIds for the motors and encoder of the swerve module */
     public static class SwerveModuleDeviceMap {
         public final DeviceId driveId, pivotId, encoderId;
         
